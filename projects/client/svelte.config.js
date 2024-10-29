664d4b8552e78202f1a50b711d5b7ce44efc808a
Vlad Jerca <vlad.jerca@gmail.com>
2024-10-29 14:07:13 +0100

    chore: hookup cloudflare adapter

--- projects/client/svelte.config.js
+++ projects/client/svelte.config.js
@@ -1,5 +1,5 @@
-import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-cloudflare';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -9,7 +9,19 @@ const config = {
 
   kit: {
     // See https://svelte.dev/docs/kit/adapters for more information about adapters.
-    adapter: adapter(),
+    adapter: adapter({
+      // See below for an explanation of these options
+      routes: {
+        include: ['/*'],
+        exclude: ['<all>'],
+      },
+      platformProxy: {
+        configPath: 'wrangler.toml',
+        environment: undefined,
+        experimentalJsonConfig: false,
+        persist: false,
+      },
+    }),
   },
 };
 
