fda63e675ad31f48b30da532f46f65f60226e190
Vlad Jerca <vlad.jerca@gmail.com>
2024-11-04 14:48:11 +0100

    chore(api): add svelte dev environments

--- projects/api/src/Environment.ts
+++ projects/api/src/Environment.ts
@@ -2,4 +2,6 @@ export enum Environment {
   production = 'https://api.trakt.tv',
   staging = 'https://api.staging.trakt.tv',
   development = 'https://api.trakt.test',
+  svelte_dev = 'http://localhost:5173/api',
+  svelte_preview = 'http://localhost:4173/api',
 }
