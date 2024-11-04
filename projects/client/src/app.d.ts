e070a31f25a03b5870177ca67b0dda29bba680bf
Vlad Jerca <vlad.jerca@gmail.com>
2024-11-04 14:48:49 +0100

    build: define network proxy and inject env variables

--- projects/client/src/app.d.ts
+++ projects/client/src/app.d.ts
@@ -1,6 +1,10 @@
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
 declare global {
+  const TRAKT_CLIENT_ID: string;
+  const TRAKT_CLIENT_SECRET: string;
+  const TRAKT_BEARER_TOKEN: string;
+
   namespace App {
     // interface Error {}
     // interface Locals {}
--- projects/client/vite.config.ts
+++ projects/client/vite.config.ts
@@ -2,9 +2,11 @@ import { paraglide } from '@inlang/paraglide-sveltekit/vite';
 import { defineConfig } from 'vitest/config';
 import { sveltekit } from '@sveltejs/kit/vite';
 import denoSveltekitExit from './.vite/deno-sveltekit-exit.ts';
+import { Environment } from '@trakt/api';
 
 import path from 'node:path';
 import fs from 'node:fs';
+import process from 'node:process';
 
 function findGitRoot(dir: string): string {
   if (fs.existsSync(path.join(dir, '.git'))) {
@@ -23,9 +25,22 @@ function findGitRoot(dir: string): string {
 const MONOREPO_ROOT = findGitRoot(__dirname);
 
 export default defineConfig({
+  define: {
+    'TRAKT_CLIENT_ID': `"${process.env.TRAKT_CLIENT_ID}"`,
+    'TRAKT_CLIENT_SECRET': `"${process.env.TRAKT_CLIENT_SECRET}"`,
+    /** TODO: remove when auth flow is part of client */
+    'TRAKT_BEARER_TOKEN': `"${process.env.TRAKT_BEARER_TOKEN}"`,
+  },
   server: {
     fs: {
       allow: [MONOREPO_ROOT],
+    },
+    proxy: {
+      '/api': {
+        target: Environment.production,
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
     },
   },
   plugins: [
