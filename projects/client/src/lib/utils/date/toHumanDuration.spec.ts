a7c3429056f2c99e94fc330be2775ec2172fd1e1
Vlad Jerca <vlad.jerca@gmail.com>
2024-11-13 23:47:59 +0100

    feat(duration): define toHumanDuration date utility

---
+++ projects/client/src/lib/utils/date/toHumanDuration.spec.ts
@@ -0,0 +1,36 @@
+import { toHumanDuration } from '$lib/utils/date/toHumanDuration.ts';
+import { describe, expect, it } from 'vitest';
+
+describe('toHumanDuration', () => {
+  it('should return a human readable duration', () => {
+    expect(toHumanDuration({ days: 1, hours: 2, minutes: 3 })).toBe('1d 2h 3m');
+  });
+
+  it('should display 300m as 5h', () => {
+    expect(toHumanDuration({ minutes: 300 })).toBe('5h');
+  });
+
+  it('should display 1440m as 1d', () => {
+    expect(toHumanDuration({ minutes: 1440 })).toBe('1d');
+  });
+
+  it('should display 1441m as 1d 1m', () => {
+    expect(toHumanDuration({ minutes: 1441 })).toBe('1d 1m');
+  });
+
+  it('should display 1500m as 1d 1h', () => {
+    expect(toHumanDuration({ minutes: 1500 })).toBe('1d 1h');
+  });
+
+  it('should display 1501m as 1d 1h 1m', () => {
+    expect(toHumanDuration({ minutes: 1501 })).toBe('1d 1h 1m');
+  });
+
+  it('should display 1530m as 1d 1h 30m', () => {
+    expect(toHumanDuration({ minutes: 1530 })).toBe('1d 1h 30m');
+  });
+
+  it('should return empty string when all parts are 0', () => {
+    expect(toHumanDuration({})).toBe('');
+  });
+});
---
+++ projects/client/src/lib/utils/date/toHumanDuration.ts
@@ -0,0 +1,47 @@
+export type Duration = {
+  days: number;
+  hours: number;
+  minutes: number;
+};
+
+export function toHumanDuration({
+  days = 0,
+  hours = 0,
+  minutes = 0,
+}, locale = 'en') {
+  hours += Math.floor(minutes / 60);
+  minutes %= 60;
+  days += Math.floor(hours / 24);
+  hours %= 24;
+
+  const formatters = {
+    day: new Intl.NumberFormat(locale, {
+      style: 'unit',
+      unit: 'day',
+      unitDisplay: 'narrow',
+    }),
+    hour: new Intl.NumberFormat(locale, {
+      style: 'unit',
+      unit: 'hour',
+      unitDisplay: 'narrow',
+    }),
+    minute: new Intl.NumberFormat(locale, {
+      style: 'unit',
+      unit: 'minute',
+      unitDisplay: 'narrow',
+    }),
+  };
+
+  const parts = [];
+  if (days > 0) {
+    parts.push(formatters.day.format(days));
+  }
+  if (hours > 0) {
+    parts.push(formatters.hour.format(hours));
+  }
+  if (minutes > 0) {
+    parts.push(formatters.minute.format(minutes));
+  }
+
+  return parts.join(' ');
+}
