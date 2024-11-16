27eba76ac0f4935b6a2fe388e116df9b1a365e4c
Vlad Jerca <vlad.jerca@gmail.com>
2024-11-16 14:24:12 +0100

    fix(profile): improve cover gradient to follow design

--- projects/client/src/lib/components/background/BackgroundCoverImage.svelte
+++ projects/client/src/lib/components/background/BackgroundCoverImage.svelte
@@ -19,25 +19,6 @@
 </div>
 
 <style>
-  :global([data-theme="light"]) {
-    .background-cover-image {
-      &.image-loaded {
-        :global(img) {
-          opacity: 0.85;
-        }
-      }
-    }
-  }
-
-  :global([data-theme="dark"]) {
-    .background-cover-image {
-      &.image-loaded {
-        :global(img) {
-          opacity: 0.32;
-        }
-      }
-    }
-  }
   .background-cover-image {
     z-index: -1;
     position: absolute;
@@ -45,6 +26,13 @@
     top: 0;
     left: 0;
     width: 100%;
+    background: var(--shade-900);
+
+    &.image-loaded {
+      :global(img) {
+        opacity: 1;
+      }
+    }
 
     :global(img) {
       width: 100%;
@@ -58,17 +46,30 @@
     &::after {
       content: "";
       width: 100%;
-      height: 50%;
+      height: 100%;
 
       position: absolute;
       bottom: 0;
       left: 0;
 
       background: linear-gradient(
-        to bottom,
-        transparent 0%,
-        color-mix(in srgb, var(--color-background) 50%, transparent 50%) 30%,
-        var(--color-background) 70%
+        180deg,
+        color-mix(in srgb, var(--color-background) 64%, transparent 36%) 0%,
+        color-mix(in srgb, var(--color-background) 65%, transparent 35%) 5%,
+        color-mix(in srgb, var(--color-background) 66%, transparent 34%) 9%,
+        color-mix(in srgb, var(--color-background) 67%, transparent 33%) 13%,
+        color-mix(in srgb, var(--color-background) 68%, transparent 32%) 17%,
+        color-mix(in srgb, var(--color-background) 69%, transparent 31%) 20%,
+        color-mix(in srgb, var(--color-background) 70%, transparent 30%) 25%,
+        color-mix(in srgb, var(--color-background) 72%, transparent 28%) 29%,
+        color-mix(in srgb, var(--color-background) 74%, transparent 26%) 34%,
+        color-mix(in srgb, var(--color-background) 77%, transparent 23%) 40%,
+        color-mix(in srgb, var(--color-background) 79%, transparent 21%) 46%,
+        color-mix(in srgb, var(--color-background) 83%, transparent 17%) 55%,
+        color-mix(in srgb, var(--color-background) 86%, transparent 14%) 64%,
+        color-mix(in srgb, var(--color-background) 90%, transparent 10%) 74%,
+        color-mix(in srgb, var(--color-background) 95%, transparent 5%) 85%,
+        var(--color-background) 100%
       );
 
       pointer-events: none;
