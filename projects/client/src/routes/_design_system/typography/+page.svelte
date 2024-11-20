55ab9544c24385699f5f0b9aec6833b6d1a2f782
Vlad Jerca <vlad.jerca@gmail.com>
2024-11-20 17:15:22 +0100

    feat(typhography): define design system route and add demo

---
+++ projects/client/src/routes/_design_system/typography/+page.svelte
@@ -0,0 +1,62 @@
+<script>
+</script>
+
+<div class="typography-disclaimer-container">
+  <p class="meta-info">(scaled to 75% for presentation)</p>
+</div>
+
+<div class="typography-element-container">
+  <span class="display-title large">A love letter to impact.</span>
+  <span class="display-title">Existentialist's guide to spacing.</span>
+
+  <h1>Lurking in the shadows of the CMYK spectrum.</h1>
+  <h2>Typography of disintegration, how Garamond lost its serif.</h2>
+  <h3>The golden re-ratio: A drunken inquiry into divine proportion.</h3>
+  <h4>Negative space: contested zone where we wrestle with nothingness.</h4>
+  <h5>
+    Font pairing: A cautionary tale, or the ballad of Helvetica and Comic Sans.
+  </h5>
+  <h6>
+    Microtype: The devil is in the details, a microscopic examination of
+    kerning.
+  </h6>
+</div>
+
+<div class="typography-element-container">
+  <p class="large">
+    The canvas stares back, a blank expanse of terror. Your palette mocks with
+    garish pigments, siren songs promising beauty but delivering despair. But
+    fear not! Within you lies the spirit of a true artist.
+  </p>
+  <p>
+    "They say Helvetica is the font of the future, a typeface so pure it
+    transcends emotion. But you know better. You've seen the cracks, the hints
+    of despair. You yearn for something with soul."
+  </p>
+  <p class="small">
+    (A lone pixel flickers, a defiant spark. Is it a glitch, a cry for help, or
+    a message from the machine gods? Only time will tell.)
+  </p>
+
+  <p class="meta-info">
+    This website was designed in a fugue state, fueled by cheap coffee and
+    dread. © The Union of Conceptual Artists, Hobo Chapter
+  </p>
+</div>
+
+<style>
+  .typography-disclaimer-container {
+    display: flex;
+    justify-content: center;
+    :global(.trakt-meta-info) {
+      font-style: italic;
+    }
+  }
+
+  .typography-element-container {
+    transform: scale(0.75);
+    display: grid;
+    gap: 1rem;
+    padding: 1rem;
+  }
+</style>
