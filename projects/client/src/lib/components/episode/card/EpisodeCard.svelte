3ce3406af4bd82bc644783a61a208ba16645c749
Vlad Jerca <vlad.jerca@gmail.com>
2024-11-20 18:35:36 +0100

    fix(episode): use correct episode card width as in design files

--- projects/client/src/lib/components/episode/card/EpisodeCard.svelte
+++ projects/client/src/lib/components/episode/card/EpisodeCard.svelte
@@ -4,6 +4,10 @@
   const { children }: ChildrenProps = $props();
 </script>
 
-<Card --width-card="12.5rem" --height-card="11rem" --height-card-cover="7.5rem">
+<Card
+  --width-card="13.25rem"
+  --height-card="11rem"
+  --height-card-cover="7.5rem"
+>
   {@render children()}
 </Card>
