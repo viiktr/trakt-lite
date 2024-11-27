75547c741288a9a212d8471d75a88af01f2566c1
seferturan <seferturan85@gmail.com>
2024-11-27 13:31:28 +0100

    fix: exclude api urls from paraglide

--- projects/client/src/lib/features/i18n/index.ts
+++ projects/client/src/lib/features/i18n/index.ts
@@ -1,7 +1,13 @@
 import * as m from '$lib/paraglide/messages.js';
 import * as runtime from '$lib/paraglide/runtime.js';
 import { createI18n } from '@inlang/paraglide-sveltekit';
-export const i18n = createI18n(runtime);
+
+export const i18n = createI18n(
+  runtime,
+  {
+    exclude: [/^\/api\//],
+  },
+);
 
 export {
   type AvailableLanguageTag,
