cdd8f774c9206c326aa0c724acb629b718a8bb1c
seferturan <seferturan85@gmail.com>
2024-11-28 13:07:36 +0100

    feat(profile): style vip badge

--- projects/client/src/lib/components/badge/VipBadge.svelte
+++ projects/client/src/lib/components/badge/VipBadge.svelte
@@ -1,22 +1,30 @@
 <script>
-  import LogoMarkCircle from "../logo/LogoMarkCircle.svelte";
+  import VipIcon from "../icons/VipIcon.svelte";
 </script>
 
 <div class="trakt-vip-badge">
-  <LogoMarkCircle --color-foreground="var(--color-foreground-vip-badge)" />
+  <VipIcon />
   <p class="small">VIP</p>
 </div>
 
 <style>
   .trakt-vip-badge {
     display: flex;
-    padding: var(--ni-4) var(--ni-8) var(--ni-4) var(--ni-4);
+    padding: var(--ni-8) var(--ni-12) var(--ni-8) var(--ni-12);
     align-items: center;
-    gap: var(--ni-4);
+    gap: var(--ni-8);
 
     border-radius: var(--ni-20);
     background: var(--color-background-vip-badge);
     color: var(--color-foreground-vip-badge);
+
+    box-shadow:
+      var(--ni-0) var(--ni-2) var(--ni-8) var(--ni-0)
+        color-mix(in srgb, var(--color-shadow) 16%, transparent 84%),
+      var(--ni-0) var(--ni-neg-2) var(--ni-4) var(--ni-0)
+        color-mix(in srgb, var(--color-shadow) 25%, transparent 75%) inset,
+      var(--ni-0) var(--ni-2) var(--ni-4) var(--ni-0)
+        color-mix(in srgb, white 44%, transparent 56%) inset;
 
     .small {
       font-weight: 700;
