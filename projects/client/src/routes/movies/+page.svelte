cceb3a15d1ba0cc0ca2cc24c44ddc4930c88bb7f
seferturan <seferturan85@gmail.com>
2024-12-11 14:15:08 +0100

    feat(movies): add initial version of the movies page

---
+++ projects/client/src/routes/movies/+page.svelte
@@ -0,0 +1,30 @@
+<script>
+  import BackgroundCoverImage from "$lib/components/background/BackgroundCoverImage.svelte";
+  import { useUser } from "$lib/features/auth/stores/useUser";
+  import RenderFor from "$lib/guards/RenderFor.svelte";
+  import RecommendedMovies from "$lib/sections/recommendations/RecommendedMovies.svelte";
+  import TrendingMovies from "$lib/sections/trending/TrendingMovies.svelte";
+  import { DEFAULT_COVER } from "$lib/utils/constants";
+
+  const { user } = useUser();
+</script>
+
+<div class="trakt-shows">
+  <RenderFor audience="authenticated">
+    <BackgroundCoverImage src={$user?.cover.url ?? DEFAULT_COVER} type="main" />
+    <TrendingMovies />
+    <!-- TODO title needs to be "Your Recommendations" -->
+    <RecommendedMovies />
+  </RenderFor>
+</div>
+
+<style>
+  .trakt-shows {
+    display: flex;
+    flex-direction: column;
+    gap: var(--ni-32);
+
+    inset: 0;
+    margin: auto;
+  }
+</style>
