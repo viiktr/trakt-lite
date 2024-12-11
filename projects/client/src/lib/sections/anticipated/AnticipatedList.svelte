991653b0dda274bee928f98298ee46e0e21169e1
seferturan <seferturan85@gmail.com>
2024-12-11 15:35:36 +0100

    feat(anticipated): anticipated movies section

---
+++ projects/client/src/lib/sections/anticipated/AnticipatedList.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import SectionList from "$lib/components/section-list/SectionList.svelte";
+  import type { MediaType } from "$lib/models/MediaType";
+  import RecommendedMedia from "../recommendations/RecommendedMedia.svelte";
+  import { useAnticipatedList } from "./stores/useAnticipatedList";
+
+  type TrendingListProps = {
+    title: string;
+    type: MediaType;
+  };
+
+  const { title, type }: TrendingListProps = $props();
+
+  const { list } = useAnticipatedList({ type });
+</script>
+
+<SectionList {title} --height-section-list="var(--height-poster-list)">
+  {#each $list as trendingItem (trendingItem.id)}
+    <RecommendedMedia {type} recommendation={trendingItem} />
+  {/each}
+</SectionList>
---
+++ projects/client/src/lib/sections/anticipated/stores/useAnticipatedList.ts
@@ -0,0 +1,42 @@
+import type { MediaType } from '$lib/models/MediaType.ts';
+import {
+  type AnticipatedMovie,
+  movieAnticipatedQuery,
+} from '$lib/requests/queries/movies/movieAnticipatedQuery.ts';
+import { createQuery, type CreateQueryOptions } from '@tanstack/svelte-query';
+import { derived } from 'svelte/store';
+
+export type AnticipatedMediaItem = AnticipatedMovie;
+export type AnticipatedMedia = Array<AnticipatedMediaItem>;
+
+const ANTICIPATED_LIMIT = 25;
+
+type AnticipatedListStoreProps = {
+  type: MediaType;
+};
+
+function typeToQuery(type: MediaType): CreateQueryOptions<AnticipatedMedia> {
+  const params = {
+    limit: ANTICIPATED_LIMIT,
+  };
+
+  switch (type) {
+    case 'movie':
+      return movieAnticipatedQuery(params);
+    case 'show':
+      throw new Error('Not implemented yet');
+    case 'episode':
+      throw new Error('Not implemented');
+  }
+}
+
+export function useAnticipatedList(
+  { type }: AnticipatedListStoreProps,
+) {
+  const query = createQuery(typeToQuery(type));
+  const list = derived(query, ($query) => $query.data ?? []);
+
+  return {
+    list,
+  };
+}
