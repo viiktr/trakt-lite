c50cff40b3fdba33349f78242270e81b9cb2fcc8
Vlad Jerca <vlad.jerca@gmail.com>
2024-12-16 21:11:12 +0100

    refactor: use correct  `./$types`

--- projects/client/src/routes/show/[slug]/+page.ts
+++ projects/client/src/routes/show/[slug]/+page.ts
@@ -1,7 +1,7 @@
 import { showSummaryQuery } from '$lib/requests/queries/shows/showSummaryQuery.ts';
-import type { ServerLoad } from '@sveltejs/kit';
+import type { PageLoad } from './$types';
 
-export const load: ServerLoad = async ({ parent, params, fetch }) => {
+export const load: PageLoad = async ({ parent, params, fetch }) => {
   const { queryClient, isBot } = await parent();
 
   if (isBot) {
