704e9d4b087bb2ddb8d904b934b52a14cb02af25
seferturan <seferturan85@gmail.com>
2024-12-21 14:10:07 +0100

    feat(tags): add meta tags

---
+++ projects/client/src/lib/components/media/tags/CertificationTag.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import TagContent from "$lib/components/tags/TagContent.svelte";
+
+  const { children }: ChildrenProps = $props();
+</script>
+
+<div class="media-certification-tag">
+  <TagContent>
+    <p class="meta-info capitalize">
+      {@render children()}
+    </p>
+  </TagContent>
+</div>
+
+<style>
+  .media-certification-tag {
+    :global(.trakt-tag) {
+      background: var(--color-background-certification-tag);
+      color: var(--color-text-certification-tag);
+    }
+  }
+</style>
---
+++ projects/client/src/lib/components/media/tags/YearTag.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import TagContent from "$lib/components/tags/TagContent.svelte";
+
+  const { children }: ChildrenProps = $props();
+</script>
+
+<div class="media-year-tag">
+  <TagContent>
+    <p class="meta-info capitalize">
+      {@render children()}
+    </p>
+  </TagContent>
+</div>
+
+<style>
+  .media-year-tag {
+    :global(.trakt-tag) {
+      background: var(--color-background-year-tag);
+      color: var(--color-text-year-tag);
+    }
+  }
+</style>
