b6e9b8a32c19d78ec82731bcdd4389fc7f5bb498
seferturan <seferturan85@gmail.com>
2024-12-23 14:45:52 +0100

    feat(tags): add media type tag

---
+++ projects/client/src/lib/components/media/tags/MediaTypeTag.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import TagContent from "$lib/components/tags/TagContent.svelte";
+
+  const { children }: ChildrenProps = $props();
+</script>
+
+<div class="media-type-tag">
+  <TagContent>
+    <p class="meta-info capitalize">
+      {@render children()}
+    </p>
+  </TagContent>
+</div>
+
+<style>
+  .media-type-tag {
+    :global(.trakt-tag) {
+      background: var(--color-background-media-type-tag);
+      color: var(--color-text-media-type-tag);
+      border-radius: var(--border-radius-xs);
+    }
+  }
+</style>
