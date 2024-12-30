87fcd13ad63343f9dd69e247c553e4d17933e21b
Vlad Jerca <vlad.jerca@gmail.com>
2024-12-30 16:17:30 +0100

    fix(summary): disable watch now until new design is in

--- projects/client/src/lib/components/buttons/watch-now/WatchNowButton.svelte
+++ projects/client/src/lib/components/buttons/watch-now/WatchNowButton.svelte
@@ -33,19 +33,28 @@
     the accessible name (https://www.w3.org/TR/wai-aria/#namecalculation)
   */
   const title = $derived(i18n.title({ title: mediaTitle, isDisabled }));
+  /**
+   * TODO: @seferturan
+   *
+   * Let's replace this watch now button with actionable tags on the cover (eg: Netflix, Prime Video, etc.)
+   * TV apps should go with actionable button, web and mobile should go with tags on the cover.
+   */
+  const isEnabled = false;
 </script>
 
-<Button
-  {title}
-  label={title}
-  href={streamingLink}
-  disabled={isDisabled || undefined}
-  style="textured"
-  color="purple"
-  target="_blank"
->
-  {i18n.text()}
-  {#snippet icon()}
-    <PlayIcon />
-  {/snippet}
-</Button>
+{#if isEnabled}
+  <Button
+    {title}
+    label={title}
+    href={streamingLink}
+    disabled={isDisabled || undefined}
+    style="textured"
+    color="purple"
+    target="_blank"
+  >
+    {i18n.text()}
+    {#snippet icon()}
+      <PlayIcon />
+    {/snippet}
+  </Button>
+{/if}
