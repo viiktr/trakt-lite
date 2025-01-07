eef1640e45143754d3f1a268e370f848b3379356
seferturan <seferturan85@gmail.com>
2025-01-07 21:21:36 +0100

    feat(coming-soon): only consider items that are actually coming

--- projects/client/src/lib/sections/lists/stores/useComingSoon.ts
+++ projects/client/src/lib/sections/lists/stores/useComingSoon.ts
@@ -1,8 +1,16 @@
 import type { MediaType } from '$lib/models/MediaType.ts';
+import type { MediaStatus } from '$lib/requests/models/MediaStatus';
 import {
   useWatchlistList,
 } from '$lib/sections/lists/stores/useWatchlistList.ts';
 import { derived } from 'svelte/store';
+
+const IN_PROGRESS_STATUSES: MediaStatus[] = [
+  'planned',
+  'post production',
+  'in production',
+  'upcoming',
+] as const;
 
 export function useComingSoon(type: MediaType) {
   const { list: watchlist, isLoading } = useWatchlistList({
@@ -13,7 +21,13 @@ export function useComingSoon(type: MediaType) {
   const list = derived(
     watchlist,
     ($watchlist) =>
-      $watchlist.filter((item) => item?.airedDate.getTime() > Date.now())
+      $watchlist
+        .filter((item) => {
+          const isUpcomingItem = item.airedDate.getTime() > Date.now();
+          const isInProgressItem = IN_PROGRESS_STATUSES.includes(item.status);
+
+          return isUpcomingItem && isInProgressItem;
+        })
         .sort((a, b) => a.airedDate.getTime() - b.airedDate.getTime()),
   );
 
