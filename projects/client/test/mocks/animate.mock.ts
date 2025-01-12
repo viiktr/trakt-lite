861509cbf058123a32f45d7ca9f7ce35ec754313
Vlad Jerca <vlad.jerca@gmail.com>
2025-01-12 10:37:45 +0100

    refactor: prefer arrow function on callback

--- projects/client/test/mocks/animate.mock.ts
+++ projects/client/test/mocks/animate.mock.ts
@@ -1,34 +1,36 @@
 import { vi } from 'vitest';
 
 if (!globalThis.Element.prototype.animate) {
-  globalThis.Element.prototype.animate = vi.fn().mockImplementation(
-    function (_, options) {
-      const animation = {
-        onfinish: null,
-        play: vi.fn(),
-        pause: vi.fn(),
-        finish: vi.fn().mockImplementation(function () {
+  globalThis.Element.prototype.animate = vi.fn()
+    .mockImplementation(
+      (_, options) => {
+        const animation = {
+          onfinish: null,
+          play: vi.fn(),
+          pause: vi.fn(),
+          finish: vi.fn()
+            .mockImplementation(() => {
+              if (animation.onfinish) {
+                (animation.onfinish as () => void)();
+              }
+            }),
+          cancel: vi.fn(),
+          reverse: vi.fn(),
+          commitStyles: vi.fn(),
+          persist: vi.fn(),
+          ready: Promise.resolve(),
+          finished: Promise.resolve(),
+          currentTime: 0,
+          playbackRate: 1,
+          startTime: 0,
+          playState: 'idle',
+        };
+        setTimeout(() => {
           if (animation.onfinish) {
             (animation.onfinish as () => void)();
           }
-        }),
-        cancel: vi.fn(),
-        reverse: vi.fn(),
-        commitStyles: vi.fn(),
-        persist: vi.fn(),
-        ready: Promise.resolve(),
-        finished: Promise.resolve(),
-        currentTime: 0,
-        playbackRate: 1,
-        startTime: 0,
-        playState: 'idle',
-      };
-      setTimeout(() => {
-        if (animation.onfinish) {
-          (animation.onfinish as () => void)();
-        }
-      }, options.duration || 0);
-      return animation;
-    },
-  );
+        }, options.duration || 0);
+        return animation;
+      },
+    );
 }
