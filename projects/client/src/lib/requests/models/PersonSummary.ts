0f5adb0b04d32691f5fdb64ff6d66598ea408203
seferturan <seferturan85@gmail.com>
2025-01-14 17:04:57 +0100

    feat(requests): get and map people summary

---
+++ projects/client/src/lib/requests/models/PersonSummary.ts
@@ -0,0 +1,5 @@
+export type PersonSummary = {
+  name: string;
+  biography: string;
+  headShotUrl: HttpsUrl;
+};
---
+++ projects/client/src/lib/requests/queries/people/peopleSummaryQuery.ts
@@ -0,0 +1,57 @@
+import type { PersonSummary } from '$lib/requests/models/PersonSummary.ts';
+import { MEDIA_POSTER_PLACEHOLDER } from '$lib/utils/constants.ts';
+import { findDefined } from '$lib/utils/string/findDefined.ts';
+import { prependHttps } from '$lib/utils/url/prependHttps.ts';
+import type { PeopleSummaryResponse } from '@trakt/api';
+import { api, type ApiParams } from '../../_internal/api.ts';
+
+type PersonSummaryParams = { slug: string } & ApiParams;
+
+function mapPeopleResponseToPersonSummary(
+  peopleSummaryResponse: PeopleSummaryResponse,
+): PersonSummary {
+  const headshotCandidate = findDefined(
+    peopleSummaryResponse.images?.headshot?.at(1),
+    peopleSummaryResponse.images?.headshot?.at(0),
+  );
+
+  return {
+    name: peopleSummaryResponse.name,
+    biography: peopleSummaryResponse.biography ?? '',
+    headShotUrl: prependHttps(
+      headshotCandidate,
+      MEDIA_POSTER_PLACEHOLDER,
+    ),
+  };
+}
+
+function peopleSummaryRequest(
+  { fetch, slug }: PersonSummaryParams,
+): Promise<PersonSummary> {
+  return api({ fetch })
+    .people
+    .summary({
+      params: {
+        id: slug,
+      },
+      query: {
+        extended: 'full,cloud9',
+      },
+    })
+    .then(({ status, body }) => {
+      if (status !== 200) {
+        throw new Error('Failed to fetch person summary');
+      }
+
+      return mapPeopleResponseToPersonSummary(body);
+    });
+}
+
+export const peopleSummaryQueryKey = (id: string) =>
+  ['peopleSummary', id] as const;
+export const peopleSummaryQuery = (
+  params: PersonSummaryParams,
+) => ({
+  queryKey: peopleSummaryQueryKey(params.slug),
+  queryFn: () => peopleSummaryRequest(params),
+});
