b02e37903ce54da3d6d49e15f514ebf2a2007e38
seferturan <seferturan85@gmail.com>
2025-01-14 11:14:53 +0100

    feat(card): add a person card component

---
+++ projects/client/src/lib/components/people/card/PersonCard.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import Card from "../../card/Card.svelte";
+
+  const { children }: ChildrenProps = $props();
+</script>
+
+<Card
+  --width-card="var(--width-person-card)"
+  --height-card="var(--height-person-card)"
+  --height-card-cover="var(--height-person-card-cover)"
+>
+  {@render children()}
+</Card>
