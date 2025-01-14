3c1889597281e779ede769f7806c96eaf204c7c3
seferturan <seferturan85@gmail.com>
2025-01-14 21:57:53 +0100

    feat(summary): add movie and show credits to people summary page

--- projects/client/src/lib/sections/summary/PeopleSummary.svelte
+++ projects/client/src/lib/sections/summary/PeopleSummary.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
+  import * as m from "$lib/features/i18n/messages.ts";
   import type { PersonSummary } from "$lib/requests/models/PersonSummary";
+  import CreditsList from "../lists/CreditsList.svelte";
   import ActorSummary from "./components/ActorSummary.svelte";
 
   const {
@@ -10,3 +12,6 @@
 </script>
 
 <ActorSummary {person} />
+
+<CreditsList title={m.movies()} type="movie" slug={person.slug} />
+<CreditsList title={m.shows()} type="show" slug={person.slug} />
