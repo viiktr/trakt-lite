4ef2979c47bc741191752aff515072fe3017428f
Vlad Jerca <vlad.jerca@gmail.com>
2025-01-18 12:18:43 +0100

    refactor(trending): use drill components

--- projects/client/src/lib/sections/lists/trending/TrendingList.svelte
+++ projects/client/src/lib/sections/lists/trending/TrendingList.svelte
@@ -1,9 +1,7 @@
 <script lang="ts">
-  import SectionList from "$lib/components/lists/section-list/SectionList.svelte";
   import type { MediaType } from "$lib/models/MediaType";
   import { UrlBuilder } from "$lib/utils/url/UrlBuilder";
-  import ViewAllButton from "../components/ViewAllButton.svelte";
-  import { mediaListHeightResolver } from "../utils/mediaListHeightResolver";
+  import DrillableMediaList from "../drilldown/DrillableMediaList.svelte";
   import TrendingMediaItem from "./TrendingMediaItem.svelte";
   import { useTrendingList } from "./useTrendingList";
 
@@ -14,24 +12,16 @@
   };
 
   const { title, drilldownLabel, type }: TrendingListProps = $props();
-
-  const { list } = useTrendingList({ type });
 </script>
 
-<SectionList
-  id={`trending-list-${type}`}
-  items={$list}
+<DrillableMediaList
   {title}
-  --height-list={mediaListHeightResolver(type)}
+  {drilldownLabel}
+  {type}
+  useList={useTrendingList}
+  urlBuilder={UrlBuilder.trending}
 >
   {#snippet item(media)}
     <TrendingMediaItem {type} {media} />
   {/snippet}
-
-  {#snippet actions()}
-    <ViewAllButton
-      href={UrlBuilder.trending({ type })}
-      label={drilldownLabel}
-    />
-  {/snippet}
-</SectionList>
+</DrillableMediaList>
