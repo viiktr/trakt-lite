e8c1351ff102351b77194bf4a499ed59ad5d1ad5
seferturan <seferturan85@gmail.com>
2025-01-19 23:54:47 +0100

    fix(summary): apply image style to poster only

--- projects/client/src/lib/components/summary/SummaryPoster.svelte
+++ projects/client/src/lib/components/summary/SummaryPoster.svelte
@@ -13,21 +13,25 @@
   const { src, alt, href, actions }: SummaryPosterProps = $props();
 </script>
 
-<div class="trakt-summary-poster">
-  <Link {href}>
-    <CrossOriginImage {src} {alt} />
-  </Link>
+<div class="trakt-summary-poster-container">
+  <div class="trakt-summary-poster">
+    <Link {href}>
+      <CrossOriginImage {src} {alt} />
+    </Link>
+  </div>
 
   {@render actions?.()}
 </div>
 
 <style>
-  .trakt-summary-poster {
+  .trakt-summary-poster-container {
     width: var(--ni-320);
     display: flex;
     flex-direction: column;
     gap: var(--ni-16);
+  }
 
+  .trakt-summary-poster {
     :global(img) {
       border-radius: var(--border-radius-xxl);
       align-self: stretch;
