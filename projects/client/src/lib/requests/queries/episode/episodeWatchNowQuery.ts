27fe0963c701f62702391c5b13a134fdc9f4a822
seferturan <seferturan85@gmail.com>
2025-01-20 00:11:50 +0100

    feat(requests): add support for fetching episode watch now links

---
+++ projects/client/src/lib/requests/queries/episode/episodeWatchNowQuery.ts
@@ -0,0 +1,37 @@
+import { api, type ApiParams } from '../../_internal/api.ts';
+import { mapWatchNowResponseToWatchNowDetails } from '../../_internal/mapWatchNowResponseToWatchNowDetails.ts';
+import type { WatchNowServices } from '../../models/WatchNowServices.ts';
+
+type EpisodeWatchNowParams = {
+  id: string;
+  country: string;
+} & ApiParams;
+
+function episodeWatchNowRequest(
+  { fetch, id, country }: EpisodeWatchNowParams,
+): Promise<WatchNowServices> {
+  return api({ fetch })
+    .episodes
+    .watchnow({
+      params: {
+        id,
+        country,
+      },
+    })
+    .then(({ status, body }) => {
+      if (status !== 200) {
+        throw new Error('Failed to fetch watch now episode');
+      }
+
+      return mapWatchNowResponseToWatchNowDetails(body, country);
+    });
+}
+
+const episodeWatchNowQueryKey = (id: string, country: string) =>
+  ['episodeWatchNow', id, country] as const;
+export const episodeWatchNowQuery = (
+  params: EpisodeWatchNowParams,
+) => ({
+  queryKey: episodeWatchNowQueryKey(params.id, params.country),
+  queryFn: () => episodeWatchNowRequest(params),
+});
