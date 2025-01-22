6ca8e38a8c75e75e35dd0ae1a485e1659a8d86db
seferturan <seferturan85@gmail.com>
2025-01-22 11:10:24 +0100

    feat(favorite): add media action for favorites

---
+++ projects/client/src/lib/sections/media-actions/favorite/FavoriteAction.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+  import FavoriteButton from "$lib/components/buttons/favorite/FavoriteButton.svelte";
+  import type { MediaType } from "$lib/models/MediaType";
+  import { onMount } from "svelte";
+  import { useFavorites } from "./useFavorites";
+
+  type FavoriteActionProps = {
+    style?: "action" | "normal";
+    title: string;
+    type: MediaType;
+    id: number;
+    onAction?: (state: boolean) => void;
+  };
+
+  const {
+    style = "action",
+    title,
+    type,
+    id,
+    onAction,
+  }: FavoriteActionProps = $props();
+
+  const {
+    isUpdatingFavorite,
+    isFavorited,
+    addToFavorites,
+    removeFromFavorites,
+  } = $derived(useFavorites({ type, id }));
+
+  onMount(() => {
+    return isUpdatingFavorite.subscribe((value) => onAction?.(value));
+  });
+</script>
+
+<FavoriteButton
+  type={style}
+  {title}
+  isFavorited={$isFavorited}
+  isFavoriteUpdating={$isUpdatingFavorite}
+  onAdd={addToFavorites}
+  onRemove={removeFromFavorites}
+/>
---
+++ projects/client/src/lib/sections/media-actions/favorite/useFavorites.ts
@@ -0,0 +1,73 @@
+import { useUser } from '$lib/features/auth/stores/useUser.ts';
+import type { MediaType } from '$lib/models/MediaType.ts';
+import { InvalidateAction } from '$lib/requests/models/InvalidateAction.ts';
+import { addToFavoritesRequest } from '$lib/requests/sync/addToFavoritesRequest';
+import { removeFromFavoritesRequest } from '$lib/requests/sync/removeFromFavoritesRequest';
+import { useInvalidator } from '$lib/stores/useInvalidator.ts';
+import { derived, writable } from 'svelte/store';
+
+export type FavoritesStoreProps = {
+  type: MediaType;
+  id: number;
+};
+
+function getFavoritesPayload({ type, id }: FavoritesStoreProps) {
+  switch (type) {
+    case 'movie':
+      return { movies: [{ ids: { trakt: id } }] };
+    case 'show':
+      return { shows: [{ ids: { trakt: id } }] };
+  }
+}
+
+export function useFavorites({ type, id }: FavoritesStoreProps) {
+  const isUpdatingFavorite = writable(false);
+  const { favorites } = useUser();
+  const { invalidate } = useInvalidator();
+
+  const isFavorited = derived(
+    favorites,
+    ($favorites) => {
+      if (!$favorites) {
+        return false;
+      }
+
+      switch (type) {
+        case 'movie':
+          return $favorites.movies.has(id);
+        case 'show':
+          return $favorites.shows.has(id);
+      }
+    },
+  );
+
+  /**
+   *  TODO: implement some sort of in-memory cache for result of action response
+   *  that will allow to show optimistic UI updates while history is being updated
+   */
+  const addOrRemoveFavorite = async (action: 'add' | 'remove') => {
+    isUpdatingFavorite.set(true);
+
+    const payload = getFavoritesPayload({ type, id });
+
+    switch (action) {
+      case 'add':
+        await addToFavoritesRequest({ body: payload });
+        break;
+      case 'remove':
+        await removeFromFavoritesRequest({ body: payload });
+        break;
+    }
+
+    await invalidate(InvalidateAction.Favorited(type));
+
+    isUpdatingFavorite.set(false);
+  };
+
+  return {
+    isUpdatingFavorite,
+    isFavorited,
+    addToFavorites: async () => await addOrRemoveFavorite('add'),
+    removeFromFavorites: async () => await addOrRemoveFavorite('remove'),
+  };
+}
