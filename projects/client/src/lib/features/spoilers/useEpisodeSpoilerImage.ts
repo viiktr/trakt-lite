2eadc921a120b742fc05cfb21fdfb8a70aa7e604
Vlad Jerca <vlad.jerca@gmail.com>
2025-01-23 23:51:20 +0100

    feat(spoiler): define episode image store

---
+++ projects/client/src/lib/features/spoilers/useEpisodeSpoilerImage.ts
@@ -0,0 +1,23 @@
+import type { EpisodeEntry } from '$lib/requests/models/EpisodeEntry';
+import type { ShowEntry as MediaEntry } from '$lib/requests/models/ShowEntry';
+import { derived } from 'svelte/store';
+import { useSpoiler } from './useSpoiler';
+
+type SpoilerImageProps = {
+  episode: EpisodeEntry;
+  show: MediaEntry;
+};
+
+export function useEpisodeSpoilerImage(props: SpoilerImageProps) {
+  const { isSpoilerHidden } = useSpoiler();
+
+  const { episode, show } = props;
+
+  return derived(
+    isSpoilerHidden,
+    ($isSpoilerHidden) =>
+      $isSpoilerHidden
+        ? show.cover.url.thumb
+        : episode.cover.url ?? show.cover.url.thumb,
+  );
+}
