ff3b4601531413ce5012639e287125774bfecea8
seferturan <seferturan85@gmail.com>
2025-02-03 19:20:56 +0100

    chore: remove old playwright test and config

--- projects/client/e2e/homepage.test.ts
+++
@@ -1,6 +0,0 @@
-import { expect, test } from '@playwright/test';
-
-test('home page has title contain Trakt', async ({ page }) => {
-  await page.goto('/');
-  expect(await page.title()).toContain('Trakt');
-});
