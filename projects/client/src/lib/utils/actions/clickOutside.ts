c1f713b521d3efd0df026bfe1f9381dc99ce9e14
seferturan <seferturan85@gmail.com>
2025-02-08 13:15:30 +0100

    fix: consider scroll and resize also as an outside click

--- projects/client/src/lib/utils/actions/clickOutside.ts
+++ projects/client/src/lib/utils/actions/clickOutside.ts
@@ -1,18 +1,26 @@
 import { GlobalEventBus } from '../events/GlobalEventBus.ts';
 
 export function clickOutside(node: HTMLElement) {
+  const dispatch = () => node.dispatchEvent(new CustomEvent('clickoutside'));
+
   function handleClick(e: MouseEvent) {
     if (!e.target) return;
     if (e.target instanceof Node && node.contains(e.target as Node)) return;
 
-    node.dispatchEvent(new CustomEvent('clickoutside'));
+    dispatch();
   }
 
-  const destroy = GlobalEventBus
-    .getInstance()
-    .register('click', handleClick);
+  const instance = GlobalEventBus.getInstance();
+
+  const destroyClick = instance.register('click', handleClick);
+  const destroyResize = instance.register('resize', dispatch);
+  const destroyScroll = instance.register('scroll', dispatch);
 
   return {
-    destroy,
+    destroy() {
+      destroyClick();
+      destroyResize();
+      destroyScroll();
+    },
   };
 }
