30f2ae80a959b9ac278047960204f906c187ecc3
seferturan <seferturan85@gmail.com>
2025-02-13 17:27:00 +0100

    feat(requests): add support for requesting user lists

---
+++ projects/client/src/lib/requests/queries/users/listItemsQuery.ts
@@ -0,0 +1,89 @@
+import type { ListedMovieResponse, ListedShowResponse } from '$lib/api.ts';
+import { defineQuery } from '$lib/features/query/defineQuery.ts';
+import { extractPageMeta } from '$lib/requests/_internal/extractPageMeta.ts';
+import {
+  mapToMovieListItem,
+  mapToShowListItem,
+} from '$lib/requests/_internal/mapToListItem.ts';
+import { api, type ApiParams } from '$lib/requests/api.ts';
+import type { MediaType } from '$lib/requests/models/MediaType.ts';
+import { PaginatableSchemaFactory } from '$lib/requests/models/Paginatable.ts';
+import { ListedMovieSchema } from '$lib/requests/queries/lists/listMovieItemsQuery.ts';
+import { ListedShowSchema } from '$lib/requests/queries/lists/listShowItemsQuery.ts';
+import { DEFAULT_PAGE_SIZE } from '$lib/utils/constants.ts';
+import { time } from '$lib/utils/timing/time.ts';
+import { z } from 'zod';
+
+type ListItemsParams =
+  & {
+    userId: string;
+    listId: string;
+    page?: number;
+    limit?: number;
+    type?: MediaType | 'movie,show';
+  }
+  & ApiParams;
+
+const ListedItemSchema = z.union([ListedMovieSchema, ListedShowSchema]);
+
+function mapToListItem(
+  listedItem: ListedMovieResponse | ListedShowResponse,
+) {
+  return listedItem.type === 'movie'
+    ? mapToMovieListItem(listedItem)
+    : mapToShowListItem(listedItem);
+}
+
+const listItemsRequest = (
+  {
+    fetch,
+    userId,
+    listId,
+    limit = DEFAULT_PAGE_SIZE,
+    page = 1,
+    type = 'movie,show',
+  }: ListItemsParams,
+) =>
+  api({ fetch })
+    .users
+    .lists
+    .items({
+      params: {
+        id: userId,
+        list_id: listId,
+        type,
+      },
+      query: {
+        extended: 'full,images',
+        page,
+        limit,
+      },
+    })
+    .then((response) => {
+      if (response.status !== 200) {
+        throw new Error('Failed to fetch user list items');
+      }
+
+      return response;
+    });
+
+export const listItemsQuery = defineQuery({
+  key: 'listItems',
+  invalidations: [],
+  dependencies: (
+    params,
+  ) => [
+    params.userId,
+    params.listId,
+    params.limit,
+    params.page,
+    params.type,
+  ],
+  request: listItemsRequest,
+  mapper: (response) => ({
+    entries: response.body.map(mapToListItem),
+    page: extractPageMeta(response.headers),
+  }),
+  schema: PaginatableSchemaFactory(ListedItemSchema),
+  ttl: time.minutes(30),
+});
