812618457b24675258ff0a61353a38a904d4ac1f
Vlad Jerca <vlad.jerca@gmail.com>
2025-02-16 16:12:21 +0100

    fix(social): define trakt page container

--- projects/client/src/routes/social/activity/+page.svelte
+++ projects/client/src/routes/social/activity/+page.svelte
@@ -1,7 +1,18 @@
 <script>
   import * as m from "$lib/features/i18n/messages";
+  import TraktPage from "$lib/sections/layout/TraktPage.svelte";
+  import TraktPageCoverSetter from "$lib/sections/layout/TraktPageCoverSetter.svelte";
 
   import SocialActivityPaginatedList from "$lib/sections/lists/social/SocialActivityPaginatedList.svelte";
+  import { DEFAULT_SHARE_COVER } from "$lib/utils/constants";
 </script>
 
-<SocialActivityPaginatedList title={m.social_activity_title()} />
+<TraktPage
+  audience="authenticated"
+  image={DEFAULT_SHARE_COVER}
+  title={m.social_activity_title()}
+>
+  <TraktPageCoverSetter />
+
+  <SocialActivityPaginatedList title={m.social_activity_title()} />
+</TraktPage>
