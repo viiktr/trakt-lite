ef7b2e931481807aacc7265072aaba253ae960a3
Vlad Jerca <vlad.jerca@gmail.com>
2025-02-19 13:15:15 +0100

    fix(summary): use standard size action

--- projects/client/src/lib/sections/lists/SeasonList.svelte
+++ projects/client/src/lib/sections/lists/SeasonList.svelte
@@ -73,7 +73,6 @@
       <MarkAsWatchedAction
         style="action"
         type="episode"
-        size="small"
         title={m.season_number_label({ number: $active.number })}
         media={$list}
         episode={$list}
