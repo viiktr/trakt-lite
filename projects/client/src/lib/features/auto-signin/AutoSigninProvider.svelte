01e7b2ad6bf0f802c3f6d102df82a0586365f802
seferturan <seferturan85@gmail.com>
2025-02-21 15:35:04 +0100

    feat(auth): add auto sign in provider

---
+++ projects/client/src/lib/features/auto-signin/AutoSigninProvider.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import { useAutoSignin } from "./useAutoSignin";
+
+  const { children }: ChildrenProps = $props();
+
+  const { isAutoSignin, redirect } = $derived(useAutoSignin());
+
+  onMount(() => {
+    if (!isAutoSignin) {
+      return;
+    }
+
+    redirect();
+  });
+</script>
+
+{#if !isAutoSignin}
+  {@render children()}
+{/if}
---
+++ projects/client/src/lib/features/auto-signin/useAutoSignin.ts
@@ -0,0 +1,38 @@
+import { goto } from '$app/navigation';
+import { page } from '$app/state';
+import { AnalyticsEvent } from '$lib/features/analytics/events/AnalyticsEvent.ts';
+import { useTrack } from '$lib/features/analytics/useTrack.ts';
+import { useAuth } from '$lib/features/auth/stores/useAuth.ts';
+import { get } from 'svelte/store';
+
+const PARAM_NAME = 'ref';
+const AUTO_SIGNIN_REF = 'trakt-og-switch';
+
+export function useAutoSignin() {
+  const { isAuthorized, url: authUrl } = useAuth();
+  const { track } = useTrack(AnalyticsEvent.EnterLite, {});
+
+  const ref = page.url.searchParams.get(PARAM_NAME);
+  const isAutoSignin = ref === AUTO_SIGNIN_REF;
+
+  const redirect = () => {
+    track();
+
+    if (!get(isAuthorized)) {
+      globalThis.window.location.assign(get(authUrl));
+      return;
+    }
+
+    const url = new URL(page.url);
+    url.searchParams.delete(PARAM_NAME);
+
+    goto(url, {
+      replaceState: true,
+    });
+  };
+
+  return {
+    isAutoSignin,
+    redirect,
+  };
+}
