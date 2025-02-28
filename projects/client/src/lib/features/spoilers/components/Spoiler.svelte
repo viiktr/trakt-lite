20032a729ecc0eb54fde92cd1bf1f19f327eaba8
Vlad Jerca <vlad.jerca@gmail.com>
2025-02-28 15:48:54 +0100

    feat(spoiler): enable show on click and improve transition

--- projects/client/src/lib/features/spoilers/components/Spoiler.svelte
+++ projects/client/src/lib/features/spoilers/components/Spoiler.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { type IsWatchedProps } from "$lib/sections/media-actions/mark-as-watched/useIsWatched";
+  import { spoilMeAnyway } from "$lib/sections/summary/components/comments/_internal/spoilMeAnyway";
   import { useSpoilerAction } from "../_internal/useSpoilerAction";
 
   const { children, ...rest }: ChildrenProps & IsWatchedProps = $props();
@@ -7,16 +8,20 @@
   const { spoiler } = useSpoilerAction(rest);
 </script>
 
-<trakt-spoiler use:spoiler>
+<trakt-spoiler use:spoiler use:spoilMeAnyway>
   {@render children()}
 </trakt-spoiler>
 
 <style>
-  :global(.trakt-spoiler) {
-    :global(p, span) {
-      transition: padding var(--transition-duration) var(--transition-timing);
+  trakt-spoiler {
+    :global(&:not(:empty):not(:has(*))),
+    :global(p:not(button p):not(a p)),
+    :global(span:not(button span):not(a span)) {
+      transition: var(--transition-increment) ease-in-out;
+      transition-property: filter, padding;
     }
-
+  }
+  :global(.trakt-spoiler) {
     /* Target elements that contain only text */
     /* Target p and span that don't have button/anchor parents */
     :global(&:not(:empty):not(:has(*))),
@@ -24,10 +29,7 @@
     :global(span:not(button span):not(a span)) {
       --blur-size: calc(var(--ni-2) * 1.5);
       filter: blur(var(--blur-size));
-
-      &:last-child {
-        padding: 0 var(--blur-size);
-      }
+      padding: 0 var(--blur-size);
     }
   }
 </style>
